70057c4bd8ada865d9707204b9675a43dd5eb2d7
thederek <mail@derek.tech>
2020-05-03 17:23:02 +0100

    Added players and first game state

--- src/index.js
+++ src/index.js
@@ -8,21 +8,24 @@ function Chip(props) {
 
 class AddPlayerForm extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {value: ""};
+    super(props);
+    this.state = { value: "" };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit(props.callBack).bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
-  handleSubmit(event) {
-    alert("Adding new player: " + this.state.value);
-    this.setState({value: ""});
-    event.preventDefault();
+  handleSubmit(parentCallBack) {
+    return (event) => {
+      event.preventDefault();
+      parentCallBack(this.state.value);
+
+      this.setState({ value: "" });
+    };
   }
 
   render() {
@@ -32,12 +35,73 @@ class AddPlayerForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
           </label>
           <input type="submit" value="Submit" />
         </form>
       </div>
     );
+  }
+}
+
+function Player(props) {
+  return <li>{props.name}</li>;
+}
+
+function Players(props) {
+  let players = props.players;
+  let playerItems = players.map((player, index) =>
+    React.createElement(Player, { key: index, name: player.name })
+  );
+  return (
+    <div>
+      <h2>Players</h2>
+      <ol>{playerItems}</ol>
+    </div>
+  );
+}
+
+class GameControl extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderAddPlayers() {
+    return (
+      <div>
+        <AddPlayerForm callBack={this.props.addPlayerCallBack} />
+        <button onClick={this.props.startGameCallBack}>Start Game</button>
+      </div>
+    );
+  }
+
+  renderPlaying() {
+    let player = this.props.currentPlayer;
+    let name = player.name + "(" + player.index + ")";
+    return (
+      <div>
+        <h2>Playing the game!</h2>
+        <p>Current turn: {player.name}</p>
+        <button disabled>Make {player.name} turn back after moving</button>
+        <br/>
+        <button>Roll the dice for {this.props.currentPlayer.name}</button>
+      </div>
+    );
+  }
+
+
+  render() {
+    if (this.props.gameState === "pregame") {
+      return this.renderAddPlayers();
+    }
+
+    if (this.props.gameState === "playing") {
+      return this.renderPlaying();
+    }
   }
 }
 
@@ -47,12 +111,14 @@ class Game extends React.Component {
     this.state = {
       chips: this.createChips(),
       players: [],
+      gameState: "pregame",
+      currentPlayer: null,
     };
   }
 
   render() {
     let chips = this.state.chips;
-    let chipsElement = chips.map((chip, index) =>
+    let chipsElements = chips.map((chip, index) =>
       React.createElement(Chip, {
         key: index,
         level: chip.level,
@@ -62,8 +128,15 @@ class Game extends React.Component {
 
     return (
       <div>
-        <div>{chipsElement}</div>
-        <AddPlayerForm/>
+        <div>{chipsElements}</div>
+        <hr />
+        <GameControl
+          gameState={this.state.gameState}
+          addPlayerCallBack={this.handleAddPlayer.bind(this)}
+          startGameCallBack={this.handleStartGame.bind(this)}
+          currentPlayer={this.state.currentPlayer}
+        />
+        <Players players={this.state.players} />
       </div>
     );
   }
@@ -84,8 +157,26 @@ class Game extends React.Component {
     return chips;
   }
 
+  getCurrentPlayer() {
+  }
+
   handleAddPlayer(name) {
-    
+    this.setState({
+      players: this.state.players.concat([
+        {
+          index: this.state.players.length,
+          name: name,
+          position: null
+        },
+      ]),
+    });
+  }
+
+  handleStartGame() {
+    this.setState({
+      gameState: "playing",
+      currentPlayer: this.state.players[0]
+    });
   }
 }
 
