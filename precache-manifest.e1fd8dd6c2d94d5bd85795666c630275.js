d5445be5fe72ffb32cf155e3c74242dce726bf12
thederek <mail@derek.tech>
2020-05-31 10:10:50 +0100

    Updates

---
+++ precache-manifest.e1fd8dd6c2d94d5bd85795666c630275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f245c051efc13d0c58cbf363c3effdcb",
+    "url": "/plunder-the-sea/index.html"
+  },
+  {
+    "revision": "7c1c98f52912b2c8b844",
+    "url": "/plunder-the-sea/static/css/main.6954a39c.chunk.css"
+  },
+  {
+    "revision": "4b9c066c675143f61880",
+    "url": "/plunder-the-sea/static/js/2.fef47e48.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/plunder-the-sea/static/js/2.fef47e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1c98f52912b2c8b844",
+    "url": "/plunder-the-sea/static/js/main.8e5c5a46.chunk.js"
+  },
+  {
+    "revision": "5c96876de8612a02fae9",
+    "url": "/plunder-the-sea/static/js/runtime-main.dd8bddce.js"
+  }
+]);
